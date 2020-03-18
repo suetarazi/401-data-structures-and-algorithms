7ebaa58f8e25b71b60f27fbbddb198538f037da5
Sue Tarazi <suemachtley@gmail.com>
2020-03-17 21:41:09 -0700

    Add project files.

---
+++ 401-data-structures-and-algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _401_data_structures_and_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
