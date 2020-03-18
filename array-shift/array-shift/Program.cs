4779603c423f9cb1d43752012be22b23ad3bc8b3
Sue Tarazi <suemachtley@gmail.com>
2020-03-17 21:50:32 -0700

    method call and basic creation

---
+++ array-shift/array-shift/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace array_shift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            InsertShiftArray();
+        }
+
+        static void InsertShiftArray()
+        {
+
+        }
+    }
+}
