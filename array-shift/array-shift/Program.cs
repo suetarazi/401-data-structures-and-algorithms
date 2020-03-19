9fec72adf1b56e682ff16379a906bf98fd487906
Sue Tarazi <suemachtley@gmail.com>
2020-03-18 22:05:37 -0700

    fixed curly brace that was missing

--- array-shift/array-shift/Program.cs
+++ array-shift/array-shift/Program.cs
@@ -14,24 +14,24 @@ namespace array_shift
         {
             Console.WriteLine("How big would you like your array? ");
             string answer = Console.ReadLine();
-            int[] userArray =  new int[Convert.ToInt32(answer)];
+            int[] userArray = new int[Convert.ToInt32(answer)];
             Console.WriteLine("What number would you like to add to the middle of the array? ");
             string answer2 = Console.ReadLine();
             int value = Convert.ToInt32(answer2);
-            
+
             int midpoint = userArray.Length / 2;
 
             int newLength = Convert.ToInt32(answer) + 1;
-            for(int i=0; i<userArray.Length; i++)
+            for (int i = 0; i < userArray.Length; i++)
             {
                 Console.WriteLine($"Please select {userArray} numbers, pressing enter after each");
                 string userNum = Console.ReadLine();
                 int arrNum = Convert.ToInt32(userNum);
                 userArray[i] = arrNum;
 
-                BuildNewArray(midpoint, newLength, userArray, value );
+                BuildNewArray(midpoint, newLength, userArray, value);
             }
-
+        }
             static int[] BuildNewArray(int midpoint, int newLength, int[] userArray, int value )
         { 
             int[] newArray = new int[newLength];
@@ -55,4 +55,4 @@ namespace array_shift
         }
     }
     }
-}
+
