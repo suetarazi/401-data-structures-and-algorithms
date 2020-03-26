c7f06f9d68b66635978e3037b96840650be25961
Sue Tarazi <suemachtley@gmail.com>
2020-03-26 13:19:48 -0700

    Initial attempt

---
+++ LLMerge/LLMerge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LLMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LLMerge(List1, List2);
+        }
+
+        static void LLMerge(Linklist List1, Linklist List2)
+        {
+            List1.Current = Head;
+            while (List1.Value != Null && List2.Value != Null)
+            
+                    
+        }
+        
+
+    }
+}
