f4934bae38cb7a10a3f4f62d30824854b198f1ed
Sue Tarazi <suemachtley@gmail.com>
2020-03-31 19:01:32 -0700

    intial animal shelter

--- Challenges/LLMerge/LLMerge/Program.cs
+++ Challenges/LLMerge/LLMerge/Program.cs
@@ -1,66 +1,37 @@
 ﻿using System;
-using node.classes;
+using LinkedList.Classes;
 
 namespace LLMerge
 {
-    public Node Head { get; set; }
-    private Node Current { get; set; }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            LinkedList list1 = new LinkedList();
             LLMerge(List1, List2);
         }
 
-        public class Linklist
+        static void LLMerge(Linklist List1, Linklist List2)
         {
-            public Node Head { get; set; }
-            private Node Current { get; set; }
+            Head = Current;
+            Node refA = List1.Current.Next;
+            Node refB = List2.Current.Next;
+            List1.Current = Head;
+            while (List1.Value != null && List2.Value != null)
+                //traverse List2 and call insert before for each node into List1
+                while (List1.next != null)
+                {
+                    List1.Current.Next = refA;
+                    List2.Current.Next = refB;
 
+                    List1.Current.Next = List2.Current;
+                    List2.Current.Next = refA;
+                    refA.Current.Next = refB;
 
-            /// <summary>
-            /// A singly linked list with adding a new node at the beginning. This has the BigO of O(1)
-            /// 1. Node is created (instantiated), 2. Value assigned to new node, 3. Head is set to this new node.
-            /// </summary>
-            /// <param name="value">the integer value of the new node to be added</param>
-            //Method to insert a new node at the beginning of the LinkedList
-            public void Insert(int value)
-            {
-                //Node instantiation
-                Node node = new Node();
+                }
 
-                //Value assigned
-                node.Value = value;
-
-                //Set next to Head
-                node.Next = Head;
-
-                //The new node is now set as Head
-                Head = node;
-
-            }
-
-
-            static void LLMerge(Linklist List1, Linklist List2)
-            {
-                Head = Current;
-                Node refA = List1.Current.Next;
-                Node refB = List2.Current.Next;
-                List1.Current = Head;
-                while (List1.Value != null && List2.Value != null)
-                    //traverse List2 and call insert before for each node into List1
-                    while (List1.next != null)
-                    {
-                        List1.Current.Next = refA;
-                        List2.Current.Next = refB;
-
-                        List1.Current.Next = List2.Current;
-                        List2.Current.Next = refA;
-                        refA.Current.Next = refB;
-
-                    }
-            }
 
 
         }
