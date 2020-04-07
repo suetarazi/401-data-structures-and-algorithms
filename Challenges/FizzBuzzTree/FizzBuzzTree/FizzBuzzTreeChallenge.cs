32c090e945ed2022bdfadee41ce50d476f7896b4
Sue Tarazi <suemachtley@gmail.com>
2020-04-07 13:11:04 -0700

    initial attempt at code

---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/FizzBuzzTreeChallenge.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTree
+{
+    class FizzBuzzTreeChallenge
+    {
+
+        public BinaryTree binaryTree(Node root)
+        {
+            BinaryTree FizzBuzzTree = new BinaryTree();
+
+            if(root.Value = null)
+            {
+                return null;
+            }
+            if(root.Value %15 == 0)
+            {
+                FizzBuzzTree.root.Value = "FizzBuzz";
+            }
+            if(root.Value %3 == 0)
+            {
+                FizzBuzzTree.root.Value = "Fizz";
+            }
+            if (root.Value % 5 == 0)
+            {
+                FizzBuzzTree.root.Value = "Buzz";
+            }
+            else
+                FizzBuzzTree.root.Value.ToString();
+        }
+
+            return FizzBuzzTree;
+
+    }
+
+}
+}
---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTree
+{
+    public class Node
+    {
+        public Node(int Value)
+        {
+            this.Value = Value;
+
+        }
+
+        public Node Left;
+        public Node Right;
+        public int Value;
+
+    }
+}
