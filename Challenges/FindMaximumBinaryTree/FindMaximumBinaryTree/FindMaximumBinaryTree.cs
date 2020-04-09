75e9693a04ff5c042f45ee90c672eefa4e59df18
Sue Tarazi <suemachtley@gmail.com>
2020-04-09 13:25:03 -0700

    initial code

---
+++ Challenges/FindMaximumBinaryTree/FindMaximumBinaryTree/FindMaximumBinaryTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FindMaximumBinaryTree.Classes;
+
+namespace FindMaximumBinaryTree
+{
+    class FindMaximumBinaryTree
+    {
+        public int BreadthFirst(BinaryTree binaryTree)
+        {
+            int max = 0;
+
+            Queue queue = new Queue();
+
+            Node node = new Node();
+
+            Node front = null;
+            Node rear = null;
+
+            int root = binaryTree.root.Value;
+            queue.Enqueue(root);
+            root.value = max;
+            
+            while (queue.Peek() != null)
+            {
+                if (front.Left != null)
+                {
+                    queue.Enqueue(front.Left.Value);
+                }
+                if (front.Right != null)
+                {
+                    queue.Enqueue(front.Right.Value);
+                }
+                
+                node.front = queue.Dequeue();
+                if (front.Value > max)
+                {
+                    front.Value = max;
+                }
+
+            }
+            return max;
+
+
+        }
+    }
+}
