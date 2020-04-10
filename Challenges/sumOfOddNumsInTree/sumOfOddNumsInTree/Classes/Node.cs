57d8a5cdb7fe4bef612a6ec8c5e2806add2f93f7
Sue Tarazi <suemachtley@gmail.com>
2020-04-10 13:23:40 -0700

    code challenge done, but no tests

---
+++ Challenges/sumOfOddNumsInTree/sumOfOddNumsInTree/Classes/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sumOfOddNumsInTree.Classes
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+}
