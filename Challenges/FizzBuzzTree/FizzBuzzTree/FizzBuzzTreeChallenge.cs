7b9347dfdd27bbf8fa431197440648fdb2b4b019
Sue Tarazi <suemachtley@gmail.com>
2020-04-11 15:14:52 -0700

    FizzBuzzTree recusion code

--- Challenges/FizzBuzzTree/FizzBuzzTree/FizzBuzzTreeChallenge.cs
+++ Challenges/FizzBuzzTree/FizzBuzzTree/FizzBuzzTreeChallenge.cs
@@ -7,33 +7,38 @@ namespace FizzBuzzTree
     class FizzBuzzTreeChallenge
     {
 
-        public BinaryTree binaryTree(BinaryTree root)
+        public void HelperBinaryTree(Node root, NodeString FizzBuzzRoot)
         {
-            BinaryTree FizzBuzzTree = new BinaryTree();
+            //   BinaryTree FizzBuzzTree = new BinaryTree();
 
-            if(root.Value = null)
+            while (root != null)
             {
-                return null;
+                if (root.Value % 15 == 0)
+                {
+                    FizzBuzzRoot.Value = "FizzBuzz";
+                }
+                if (root.Value % 3 == 0)
+                {
+                    FizzBuzzRoot.Value = "Fizz";
+                }
+                if (root.Value % 5 == 0)
+                {
+                    FizzBuzzRoot.Value = "Buzz";
+                }
+                else
+                    FizzBuzzRoot.Value.ToString();
             }
-            if(root.Value %15 == 0)
-            {
-                FizzBuzzTree.root.Value = "FizzBuzz";
-            }
-            if(root.Value %3 == 0)
-            {
-                FizzBuzzTree.root.Value = "Fizz";
-            }
-            if (root.Value % 5 == 0)
-            {
-                FizzBuzzTree.root.Value = "Buzz";
-            }
-            else
-                FizzBuzzTree.root.Value.ToString();
+           
         }
 
+
+
+        public StringBinaryTree ReturnTree(BinaryTree binaryTree)
+        {
+            StringBinaryTree FizzBuzzTree = new StringBinaryTree();
+            HelperBinaryTree(binaryTree.root, FizzBuzzTree.root);
+
             return FizzBuzzTree;
-
+        }
     }
-
-}
 }
---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/NodeString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTree
+{
+    public class NodeString
+    {
+        public NodeString(string Value)
+        {
+            this.Value = Value;
+
+        }
+
+        public Node Left;
+        public Node Right;
+        public string Value;
+    }
+}
