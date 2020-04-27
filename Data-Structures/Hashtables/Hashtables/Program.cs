dfbaaa99c8b49905d11fcca75e7e95224051f51e
Sue Tarazi <suemachtley@gmail.com>
2020-04-27 11:50:46 -0700

    comment

--- Data-Structures/Hashtables/Hashtables/Program.cs
+++ Data-Structures/Hashtables/Hashtables/Program.cs
@@ -4,6 +4,10 @@ namespace Hashtables
 {
     class Program
     {
+        /// <summary>
+        /// public can be a place to call methods; but they are being tested
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
