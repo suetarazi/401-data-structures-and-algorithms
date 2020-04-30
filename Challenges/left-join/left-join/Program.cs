35f37ce62436749758cf3e12c445003e85113e2a
Sue Tarazi <suemachtley@gmail.com>
2020-04-29 22:10:17 -0700

    initial code

---
+++ Challenges/left-join/left-join/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace left_join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
