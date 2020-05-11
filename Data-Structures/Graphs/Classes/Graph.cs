d51c98bf52a39ebd11c25bcbebd89a85f5ce0b50
Sue Tarazi <suemachtley@gmail.com>
2020-05-10 20:48:30 -0700

    Graphs - initial code

---
+++ Data-Structures/Graphs/Classes/Graph.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Graph<T>
+{
+	public Dictionary<Vertex<T>, List<Edge<T>>> AdjacencyList { get; set; }
+
+	private int _size;
+
+	public Graph()
+	{
+		AdjacencyList = new Dictionary<Vertex<T>, List<Edge<T>>>();
+	}
+
+	// Add Node/Vertex to the graph/Adjacency List
+
+	public Vertex<T> AddNode(T value)
+    {
+		//step 1: create node
+		Vertex<T> node = new Vertex<T>(value);
+		//step2: add to adjacency list
+		AdjacencyList.Add(node, new List<Edge<T>>());
+		_size++;
+		return node;
+    }
+
+	//add directed edge:
+	public void AddDirectedEdge(Vertex<T> a, Vertex<T> b, int weight)
+    {
+		AdjacencyList[a].Add(
+			new Edge<T>
+			{
+				Weight = weight,
+				Vertex = b
+			});
+
+    }
+
+	//add undirected edge:
+	public void AddUndirectedEdge(Vertex<T> a, Vertex<T> b, int weight)
+    {
+		AddDirectedEdge(a, b, weight);
+		AddDirectedEdge(b, a, weight);
+    }
+
+	public List<Vertex<T>> GetAllVertices()
+    {
+		//they all live inside adjacency list at the moment
+		//traverse all nodes in list and output them
+		List<Vertex<T>> vertices = new List<Vertex<T>>();
+
+		foreach(var vertex in AdjacencyList)
+        {
+			vertices.Add(vertex.Key);
+        }
+		return vertices;
+    }
+
+	//get neighbors of a given vertex
+	public List<Edge<T>> GetNeighbors(Vertex<T> vertex)
+    {
+		//get all edges connected to a vertex
+		return AdjacencyList[vertex];
+    }
+
+	//return the total number of vertices
+	public int Size()
+    {
+		return _size;
+    }
+
+	public void Print()
+    {
+		foreach(var vertex in AdjacencyList)
+        {
+			Console.Write($"{vertex.Key.Value} : ");
+
+			foreach(var edge in vertex.Value)
+            {
+				ConsoleWrite($"{edge.Vertex.Value}, {edge.Weight} -> ");
+            }
+
+			Console.WriteLine(" null");
+        }
+    }
+}
---
+++ Data-Structures/Graphs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void GraphExample()
+            {
+                Graph<string> graph = new Graph<string>();
+
+                var nd = graph.AddNode("North Dakota");
+                var mt = graph.AddNode("Montana");
+                var ga = graph.AddNode("Georgia");
+                var ca = graph.AddNode("California");
+                var wa = graph.AddNode("Washington");
+
+                graph.AddDirectedEdge(wa, nd, 125);
+                graph.AddUndirectedEdge(ca, mt, 100);
+                graph.AddDirectedEdge(ga, mt, 72);
+                graph.AddUndirectedEdge(wa, ca, 11);
+                graph.AddDirectedEdge(nd, mt, 7);
+
+                graph.Print();
+
+                var neighbors = graph.GetNeighbors(wa);
+
+                var getAll = graph.GetAllVertices();
+            }
+    }
+}
