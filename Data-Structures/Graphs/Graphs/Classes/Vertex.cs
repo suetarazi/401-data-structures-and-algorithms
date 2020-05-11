6f70f378a2e6e10baf0ecdd914279daa8b0ce74f
Sue Tarazi <suemachtley@gmail.com>
2020-05-10 22:46:58 -0700

    graphs implementation - take2

---
+++ Data-Structures/Graphs/Graphs/Classes/Vertex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs.Classes
+{
+    class Vertex<T>
+    {
+        public T Value { get; set; }
+
+        public Vertex(T value)
+        {
+            Value = value;
+        }
+    }
+}
