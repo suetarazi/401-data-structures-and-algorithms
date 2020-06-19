034b91c34e45fa95d41cd74282d3739b639691ff
Sue Tarazi <suemachtley@gmail.com>
2020-06-18 21:48:03 -0700

    bubble sort code working!

--- Challenges/BubbleSort/BubbleSort/Program.cs
+++ Challenges/BubbleSort/BubbleSort/Program.cs
@@ -14,39 +14,35 @@ namespace BubbleSort
 
         public int[] BubbleSortMethod(int[] inputArr)
         {
-            int[] sortedArr = new int[inputArr.Length];
+            //int[] sortedArr = new int[inputArr.Length];
 
-            for (int i = 0, j = 0; i < 7; i++, j++)
-            {
-
-            }
-
+            
             for (int i = 0; i < inputArr.Length - 1; i++)
             {
                 for (int j = 0; j < inputArr.Length - 1 - i; j++)
                 {
-                    //Console.WriteLine($"input i is {inputArr[i]} and input j is {inputArr[j]}");
-                    if (inputArr[i] > inputArr[i+1])
+               
+                    if (inputArr[j] > inputArr[j+1])
                     {
-                        var temp = inputArr[j];
-                        inputArr[j] = inputArr[i];
-                        inputArr[i] = temp;
+                        var temp = inputArr[j+1];
+                        inputArr[j+1] = inputArr[j];
+                        inputArr[j] = temp;
 
                     }
-                    else if (inputArr[i] <= inputArr[j])
+                    else if (inputArr[j] <= inputArr[j+1])
                     {
 
                     }
                 }
             }
 
-            inputArr.CopyTo(sortedArr, 0);
+            //inputArr.CopyTo(sortedArr, 0);
             foreach(int num in inputArr)
             {
-                sortedArr = inputArr;
+              //  sortedArr = inputArr;
                 Console.WriteLine(num);
             }
-                return sortedArr;
+                return inputArr;
         }
     }
 }
