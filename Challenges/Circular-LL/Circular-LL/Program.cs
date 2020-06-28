0e63d6e361e898a5c3d71fe8fb049e657c7812d5
Sue Tarazi <suemachtley@gmail.com>
2020-06-27 21:41:33 -0700

    code

--- Challenges/Circular-LL/Circular-LL/Program.cs
+++ Challenges/Circular-LL/Circular-LL/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Transactions;
 
 namespace Circular_LL
 {
@@ -46,6 +48,12 @@ namespace Circular_LL
 
         public bool LLCircularTwoPointers(LinkedList<int> input)
         {
+            LinkedListNode<int>.current = LinkedListNode<int>.head;
+            //input.Current = input.Head;
+            temp = Head;
+
+            current = current.next;
+
 
         }
     }
