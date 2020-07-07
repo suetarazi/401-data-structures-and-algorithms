88e55e9fe340a42daaaa566f88fba618ecb577e8
Sue Tarazi <suemachtley@gmail.com>
2020-07-07 16:50:55 -0700

    still  a few issues with syntax

--- Challenges/Circular-LL/Circular-LL/Node.cs
+++ Challenges/Circular-LL/Circular-LL/Node.cs
@@ -4,11 +4,13 @@ using System.Text;
 
 namespace Circular_LL
 {
-    class Node
+    public class Node
     {
         public Node Current { get; set; }
         public Node Next { get; set; }
 
         public Node Head { get; set; }
+
+        public Node Temp { get; set; }
     }
 }
