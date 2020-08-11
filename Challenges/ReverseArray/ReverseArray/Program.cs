f3b6a4785c0b8238a77c6b6bad030a1f59d142ab
Sue Tarazi <suemachtley@gmail.com>
2020-08-11 13:29:07 -0700

    reversed array using two methods

--- Challenges/ReverseArray/ReverseArray/Program.cs
+++ Challenges/ReverseArray/ReverseArray/Program.cs
@@ -7,6 +7,23 @@ namespace ReverseArray
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+        }
+
+        public int[] ArrayReverse1(int[] arr)
+        {
+            for(int i = arr.Length; i >= 0; i--)
+            {
+                arr[i] = i;
+            }
+
+            return arr;
+        }
+
+        public int[] ArrayReverse2(int[] arr)
+        {
+            Array.Reverse(arr);
+            return arr;
         }
     }
 }
